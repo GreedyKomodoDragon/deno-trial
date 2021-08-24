a5b51733f4e0e934b26ce471c70e31ff459afb87
GreedyKomodoDragon <jakeychap@gmail.com>
2021-08-24 12:35:39 +0100

    Adding complete tutorial with crediting

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # deno-trial
 Repository to learn Deno
+
+Tutorial and credit at: https://www.sanity.io/guides/server-side-rendering-deno-react
---
+++ frontend/ssr/client.tsx
@@ -0,0 +1,10 @@
+// frontend/ssr/client.tsx
+import { React, ReactDOM, BrowserRouter } from "../dep.ts";
+import App from "../components/App.tsx";
+
+ReactDOM.hydrate(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
